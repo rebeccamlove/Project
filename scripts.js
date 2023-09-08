70638e3ad6f63063d4055d1cf7146a3dce5053ae
Rebecca Love <123664295+rebeccamlove@users.noreply.github.com>
2023-09-08 14:43:56 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+<script>
+        $(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('affix');
+            }
+        });
+      //additional js//
+      $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+    </script>
